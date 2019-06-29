00d8efcbb68632b18255c15e58999b74d356c21e
xz <ldwqh0@outlook.com>
2019-06-29 12:02:54 +0800

    增加表格列表页面

---
+++ web/src/module/table/component/Tables.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-container class="edc-tables">
+    <el-aside>侧边</el-aside>
+    <el-main>内容</el-main>
+  </el-container>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import { Component } from 'vue-property-decorator'
+
+  @Component
+  export default class Tables extends Vue {
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
