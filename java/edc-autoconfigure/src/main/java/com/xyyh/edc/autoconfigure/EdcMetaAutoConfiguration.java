f3b08886ad23929c3adb37080613baa1315f641b
xz <ldwqh0@outlook.com>
2019-07-16 16:01:59 +0800

    增加mysql实现

--- java/edc-autoconfigure/src/main/java/com/xyyh/edc/autoconfigure/EdcMetaAutoConfiguration.java
+++ java/edc-autoconfigure/src/main/java/com/xyyh/edc/autoconfigure/EdcMetaAutoConfiguration.java
@@ -1,10 +1,22 @@
 package com.xyyh.edc.autoconfigure;
 
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.xyyh.edc.meta.entity.Table;
 
 @EntityScan({ "com.xyyh.edc.meta.entity" })
@@ -12,5 +24,25 @@ import com.xyyh.edc.meta.entity.Table;
 @ComponentScan({ "com.xyyh.edc.meta" })
 @ConditionalOnClass(Table.class)
 public class EdcMetaAutoConfiguration {
+
+	@Configuration
+	@ConditionalOnClass(ObjectId.class)
+	class ObjectMapperConfig {
+		@Autowired
+		private ObjectMapper objectMapper;
+
+		@PostConstruct
+		public void configObjectMapper() {
+			SimpleModule module = new SimpleModule("ObjectIdmodule");
+			module.addSerializer(ObjectId.class, new JsonSerializer<ObjectId>() {
+				@Override
+				public void serialize(ObjectId value, JsonGenerator gen, SerializerProvider serializers)
+						throws IOException {
+					gen.writeString(value.toHexString());
+				}
+			});
+			objectMapper.registerModule(module);
+		};
+	}
 
 }
