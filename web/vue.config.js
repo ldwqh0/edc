f230f9a151b37868515ff30304f8ea26e5d16ea0
LiDong <ldwqh0@outlook.com>
2019-09-25 15:26:27 +0800

    修改反向代理

--- web/vue.config.js
+++ web/vue.config.js
@@ -4,50 +4,13 @@ module.exports = {
   publicPath: process.env.CONTEXT_PATH,
   assetsDir: 'static',
   devServer: {
+    port: 80,
     proxy: {
-      // 如果使用本地后台服务，请打开下列配置
-      // 数据中心
-      // '/dc/d/data/': {
-      //   changeOrigin: true,
-      //   target: 'http://127.0.0.1:8081/',
-      //   pathRewrite: { '^/dc/d/data/': '' }
-      // },
-      // 可视化配置
-      // '/dc/d/dvm/': {
-      //   changeOrigin: true,
-      //   target: 'http://127.0.0.1:8082/',
-      //   pathRewrite: { '^/dc/d/dvm/': '' }
-      // },
-      // 应急指挥
-      '/dc/d/ioc/': {
-        changeOrigin: true,
-        target: 'http://127.0.0.1:8083/',
-        pathRewrite: { '^/dc/d/ioc/': '' }
-      },
-      '/dc/d/gather/': {
-        changeOrigin: true,
-        target: 'http://127.0.0.1:8084/',
-        pathRewrite: { '^/dc/d/gather/': '' }
-      },
-      // 数据相关地址
-      '/dc/d/': {
-        changeOrigin: true,
-        target: 'http://ldwqh0.kmdns.net:280',
-      },
-      // 用户管理相关地址
-      '/dc/u/': {
-        changeOrigin: true,
-        target: 'http://ldwqh0.kmdns.net:280',
-      },
-      // 权限管理相关地址
-      '/dc/p/': {
-        changeOrigin: true,
-        target: 'http://ldwqh0.kmdns.net:280',
-      },
       // 认证的地址，单独填写
-      '/oauth/': {
+      '/edc/d/': {
         changeOrigin: true,
-        target: 'http://ldwqh0.kmdns.net:280',
+        target: 'http://localhost:8080:8080/',
+        path
       }
     },
   },
